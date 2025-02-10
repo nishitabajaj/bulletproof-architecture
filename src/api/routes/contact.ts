846cc2d124932544a75ec8145ef9520d4a4a5a2d
Nishita Bajaj <bajajnishita987@gmail.com>
2025-02-10 15:38:33 +0530

    Revert "Updated contact.ts and added server.ts"
    This reverts commit 55bd36c7a4a03403adc241a5b850b03fdd1b33fa.

--- src/api/routes/contact.ts
+++ src/api/routes/contact.ts
@@ -7,21 +7,23 @@ const route = Router();
 export default (app: Router) => {
   app.use('/contact', route);
   const contactService = Container.get(ContactService);
-
   route.get('/', middlewares.isAuth, middlewares.attachCurrentUser, (req: Request, res: Response) => {
     return res.json({ user: req.currentUser }).status(200);
   });
 
   route.post('/', async (req: any, res: Response, next: NextFunction) => {
+    
     const dto = contactService.contactCreateDto(req)
     const data = contactService.contactCreate(dto)
     return res.json({ data }).status(201)
-  });
+  })
 
   route.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
     try {
+      
       const contactId = req.params.id;
-      const updateDto = contactService.contactUpdateDto(req);  
+      const updateDto = contactService.contactUpdateDto(req);
+  
       const updatedContact = await contactService.contactUpdate(contactId, updateDto);
   
       if (!updatedContact) {
